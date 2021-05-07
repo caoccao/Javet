701dadbb3edc884a254babf4717c8c196cdcbda4
sam.cao <sam.cao@mchrcloud.com>
2021-05-07 10:28:44 +0800

    Added Javet rebuild for sqlite3 on Linux

---
+++ scripts/node/javet-rebuild/rebuild-sqlite3.sh
@@ -0,0 +1,1 @@
+export NODE_MODULE_FILE="../node_modules/sqlite3/lib/binding/napi-v3-linux-x64/node_sqlite3.node"
---
+++ scripts/node/javet-rebuild/rebuild.sh
@@ -0,0 +1,1 @@
+patchelf --add-needed libjavet-node-linux-x86_64.v.0.8.7.so ${NODE_MODULE_FILE}
