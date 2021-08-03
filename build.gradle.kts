2c8b202c1a25289f2f4a96668df5b3ab962728f7
Sam Cao <sjtucaocao@gmail.com>
2021-08-03 08:19:26 +0800

    Upgraded version to 0.9.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 }
 
 group = "com.caoccao.javet"
-version = "0.9.7"
+version = "0.9.8"
 
 repositories {
     mavenCentral()
--- scripts/python/change_javet_version.py
+++ scripts/python/change_javet_version.py
@@ -109,7 +109,7 @@ class ChangeJavetVersion(object):
       logging.info('  Updated.')
 
 def main():
-  change_javet_version = ChangeJavetVersion('0.9.7')
+  change_javet_version = ChangeJavetVersion('0.9.8')
   change_javet_version.update()
   return 0
 
