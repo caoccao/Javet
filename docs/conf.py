b0a73337b1d6ff1642ffde166d1ed5ee66ffdbd0
Sam Cao <sjtucaocao@gmail.com>
2021-09-28 09:58:15 +0800

    Refactored docs

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,8 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 html_theme = 'furo'
+html_show_sphinx = False
+html_copy_source = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
