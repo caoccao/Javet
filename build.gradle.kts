10ab1fc3ee15043ffddae3ae665b84c81040828c
Sam Cao <sjtucaocao@gmail.com>
2021-09-30 15:39:12 +0800

    Upgraded version to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 }
 
 group = "com.caoccao.javet"
-version = "0.9.14"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021. caoccao.com Sam Cao'
 author = 'Sam Cao'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.14'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
