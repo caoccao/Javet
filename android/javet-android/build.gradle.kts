bfa89abb40c964fe3cc09482cfe66a12bc051567
Sam Cao <sjtucaocao@gmail.com>
2021-10-24 18:01:40 +0800

    🦄 refactor(Build): Remove redundant dependency from Android

--- android/javet-android/build.gradle.kts
+++ android/javet-android/build.gradle.kts
@@ -55,7 +55,6 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
