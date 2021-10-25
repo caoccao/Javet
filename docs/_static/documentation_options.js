226616963652de62e479eba96ee8fb8e3156c277
Sam Cao <sjtucaocao@gmail.com>
2021-10-25 14:10:43 +0800

    🔧 build(Version): Upgrade to v1.0.3

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2',
+    VERSION: '1.0.3',
     LANGUAGE: 'en, zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
