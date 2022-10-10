5fab6b537a7789da6a04951ed63f53d274085d6a
Sam Cao <sjtucaocao@gmail.com>
2022-10-10 15:10:21 +0800

    🔧 build(Version): Update version to v2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 }
 
 group = "com.caoccao.javet"
-version = "2.0.0"
+version = "2.0.1"
 
 dependencies {
     testImplementation("org.eclipse.jetty.websocket:websocket-server:9.4.44.v20210927")
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021-2022. caoccao.com Sam Cao'
 author = 'Sam Cao'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
