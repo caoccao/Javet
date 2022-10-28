21855147be15662c8bb6ab9b20c9b9447ca29f33
Sam Cao <sjtucaocao@gmail.com>
2022-10-28 09:28:29 +0800

    🐳 chore(Version): Update version to v2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 }
 
 group = "com.caoccao.javet"
-version = "2.0.1"
+version = "2.0.2"
 
 dependencies {
     testImplementation("org.eclipse.jetty.websocket:websocket-server:9.4.49.v20220914")
