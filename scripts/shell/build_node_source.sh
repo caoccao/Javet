d13c4dc4017c3923f98f29c399a5830b73ee4d2e
Sam Cao <sjtucaocao@gmail.com>
2022-12-09 12:04:22 +0800

    🐞 fix(Workflow): Fix python3 not found 4

--- scripts/shell/build_node_source.sh
+++ scripts/shell/build_node_source.sh
@@ -3,9 +3,9 @@
 set -euxo pipefail
 
 build_node(){
-	/usr/bin/python3 patch_node_build.py -p ./
+	python3 patch_node_build.py -p ./
 	./configure --enable-static --without-intl
-	/usr/bin/python3 patch_node_build.py -p ./
+	python3 patch_node_build.py -p ./
 	rm patch_node_build.py
 	make -j4
 	echo "Node.js build is completed"
