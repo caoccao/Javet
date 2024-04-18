727d9335a6dad3c0ef431be08eef81599d855493
Sam Cao <sjtucaocao@gmail.com>
2024-04-18 10:23:59 +0800

    📃 docs: Update docs for release

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Javet'
-copyright = '2021-2023. caoccao.com Sam Cao'
+copyright = '2021-2024. caoccao.com Sam Cao'
 author = 'Sam Cao'
 
 # The full version, including alpha/beta/rc tags
