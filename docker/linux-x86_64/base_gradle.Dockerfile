cfca3b21a21a16073e3670fec75261591634e0dc
Sam Cao <sjtucaocao@gmail.com>
2024-07-24 07:18:49 +0800

    🐳 chore: Update version to v3.1.5

--- docker/linux-x86_64/base_gradle.Dockerfile
+++ docker/linux-x86_64/base_gradle.Dockerfile
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Usage: docker build \
-#  -t sjtucaocao/javet:x86_64-3.1.4 \
+#  -t sjtucaocao/javet:x86_64-3.1.5 \
 #  --build-arg JAVET_REPO=sjtucaocao/javet \
 #  -f docker/linux-x86_64/base_gradle.Dockerfile .
 
--- scripts/python/change_javet_version.py
+++ scripts/python/change_javet_version.py
@@ -186,7 +186,7 @@ class ChangeJavetVersion(object):
       logging.info('  Updated.')
 
 def main():
-  change_javet_version = ChangeJavetVersion('3.1.4')
+  change_javet_version = ChangeJavetVersion('3.1.5')
   change_javet_version.update()
   return 0
 
