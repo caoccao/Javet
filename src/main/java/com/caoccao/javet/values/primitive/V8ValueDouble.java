c826dbeac68b1a551a626984613d1ea461d6f53e
xiaoye20185 <1923834375@qq.com>
2024-08-07 22:27:52 +0800

    delete native convert double type

--- src/main/java/com/caoccao/javet/values/primitive/V8ValueDouble.java
+++ src/main/java/com/caoccao/javet/values/primitive/V8ValueDouble.java
@@ -128,23 +128,6 @@ public final class V8ValueDouble
 
     @Override
     public String toString() {
-        return toString(10);
-    }
-
-    /**
-     * Number.prototype.toString()
-     * The toString() method of Number values returns a string representing this number value.
-     *
-     * @param radix An integer in the range 2 through 36 specifying the base to use for representing the number value.
-     *              Defaults to 10.
-     * @return the string
-     * @since 3.1.3
-     */
-    public String toString(int radix) {
-        try {
-            return checkV8Runtime().getExecutor("(" + value + ").toString(" + radix + ")").executeString();
-        } catch (JavetException e) {
-            return value.toString();
-        }
+        return value.toString();
     }
 }
