572a858f2a16dbfa88670d86651bdfc1699e9e71
Sam Cao <sjtucaocao@gmail.com>
2025-11-16 18:05:47 +0100

    📃 docs: Upgrade to sphinx-tabs

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '5.0.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_inline_tabs',
+    'sphinx_tabs.tabs',
     'sphinx.ext.extlinks',
 ]
 
